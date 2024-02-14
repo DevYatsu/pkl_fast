cf87ca24d6f1c23ae328a95bbeb0c4281af124ee
Yatsu <yatsu.dev@gmail.com>
2024-02-14 02:28:36 +0100

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use logos::{Lexer, Logos};
-use std::{env, fs, time::Instant};
+use std::{env, fs, ops::Index, time::Instant};
 
 use pkl_fast::lexer::PklToken;
 
@@ -8,13 +8,15 @@ mod parser;
 
 fn main() -> miette::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let target_file = if args.len() > 1 {
-        let first_argument = &args[1];
-        println!("First argument: {}", first_argument);
-        args[1].to_owned()
-    } else {
-        println!("No target file provided. Run `cargo run <file_name>`");
-        return Ok(());
+    let target_file = match args.get(1) {
+        Some(value) => {
+            println!("Last argument: {}", value);
+            value.to_owned()
+        }
+        None => {
+            println!("No target file provided. Run the command `cargo run <file_name>`");
+            return Ok(());
+        }
     };
 
     let pkl_code = fs::read_to_string(target_file).unwrap_or("".to_owned());
@@ -23,7 +25,7 @@ fn main() -> miette::Result<()> {
 
     let statements = parse(lexer)?;
     println!("{:?}", statements);
-    
+
     let end = Instant::now();
     println!("Total time: {} microseconds", (end - start).as_micros());
 
