15e9c7ab8f0b8a4ce0e0553c609e7cdb350ba926
Yatsu <yatsu.dev@gmail.com>
2024-02-25 22:23:03 +0100

    utils file removed as not needed

--- src/parser/utils.rs
+++
@@ -1,8 +0,0 @@
-use super::{
-    errors::{
-        locating::{generate_source, get_error_location},
-        UnexpectedError,
-    },
-    ParsingError, ParsingResult, PklLexer,
-};
-use crate::lexer::PklToken;
