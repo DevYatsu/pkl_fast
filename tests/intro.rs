69cee4e0f445b732ebf6c6ae62c4fc221bb1bded
Yatsu <yatsu.dev@gmail.com>
2024-02-26 17:26:07 +0100

    intro test add

---
+++ tests/intro.rs
@@ -0,0 +1,15 @@
+use pkl_fast::{parser::parse, prelude::lex};
+
+#[test] 
+fn intro_docs_example() {
+    const SOURCE: &str = "name = \"Pkl: Configure your Systems in New Ways\"
+attendants = 100
+isInteractive = true
+amountLearned = 13.37";
+
+    let tokens = lex(SOURCE);
+    let statements = parse(tokens);
+
+    assert_eq!(statements.is_ok(), true);
+    assert_eq!(statements.unwrap().len() == SOURCE.lines().count(), true)
+}
