a5d576e2e5debef928f3d53a62b6d92cd4aa2c8b
Yatsu <yatsu.dev@gmail.com>
2024-02-26 22:38:43 +0100

    tests updated

--- tests/localhost.rs
+++ tests/localhost.rs
@@ -8,13 +8,15 @@ port = 3599
 
 environment = \"dev\"
 
+/*
 database {
   host = \"localhost\"
   port = 5786
   username = \"admin\"
   password = read(\"env:DATABASE_PASSWORD\") 
   dbName = \"myapp\"
-}";
+}
+*/";
 
 #[test]
 fn localhost_doc_example() {
--- tests/var.rs
+++ tests/var.rs
@@ -34,3 +34,35 @@ num4 = 0o0134_6475";
     assert_eq!(statements.is_ok(), true);
     assert_eq!(statements.unwrap().len() == S.lines().count(), true)
 }
+
+#[test]
+fn duration_and_datasize() {
+    const S: &str = "duration1 = 5.ns  // nanoseconds (smallest unit)
+duration2 = 5.us  // microseconds
+duration3 = 5.ms  // milliseconds
+duration4 = 5.s   // seconds
+duration5 = 5.min // minutes
+duration6 = 5.h   // hours
+duration7 = 3.d   // days (largest unit)
+duration8 = 1.6666666666666667.min
+
+datasize1 = 5.b  // bytes (smallest unit)
+datasize2 = 5.kb // kilobytes
+datasize3 = 5.mb // megabytes
+datasize4 = 5.gb // gigabytes
+datasize5 = 5.tb // terabytes
+datasize6 = 5.pb // petabytes (largest unit)
+datasize1 = 5.b   // bytes (smallest unit)
+datasize2 = 5.kib // kibibytes
+datasize3 = 5.mib // mebibytes
+datasize4 = 5.gib // gibibytes
+datasize5 = 5.tib // tebibytes
+datasize6 = 5.pib // pebibytes (largest unit)
+datasize7 = 1.6666666666666667.mb
+";
+
+    let tokens = lex(S);
+    let statements = parse(tokens);
+
+    assert_eq!(statements.is_ok(), true);
+}
