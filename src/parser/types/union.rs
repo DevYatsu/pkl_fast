2e76c2a4a3c0b7b0982185b3f4f60abd60f33db9
Yatsu <yatsu.dev@gmail.com>
2024-03-04 23:05:52 +0100

    type alias parsing working nicely

---
+++ src/parser/types/union.rs
@@ -0,0 +1,44 @@
+use crate::{
+    parser::utils::retrieve_opt_next_token,
+    prelude::{ParsingResult, PklLexer, PklToken},
+};
+
+use super::{parse_type, PklType};
+
+pub fn parse_opt_union<'source>(
+    lexer: &mut PklLexer<'source>,
+    mut base_type: PklType<'source>,
+) -> ParsingResult<(PklType<'source>, Option<PklToken<'source>>)> {
+    let token = retrieve_opt_next_token(lexer)?;
+
+    let result = match token {
+        Some(PklToken::UnionSerarator) => {
+            let (t, next_token) = parse_type(lexer)?;
+
+            if let PklType::Union(ref mut values) = base_type {
+                match t {
+                    PklType::Union(second_values) => {
+                        values.extend(second_values);
+                    }
+                    _ => values.push(t),
+                };
+
+                (base_type, next_token)
+            } else {
+                let mut values = vec![base_type];
+
+                match t {
+                    PklType::Union(second_values) => {
+                        values.extend(second_values);
+                    }
+                    _ => values.push(t),
+                };
+
+                (PklType::Union(values), next_token)
+            }
+        }
+        _ => (base_type, token),
+    };
+
+    Ok(result)
+}
