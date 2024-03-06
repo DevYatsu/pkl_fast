7e8f9de836232cd1244c7055633f564e460e8511
Yatsu <yatsu.dev@gmail.com>
2024-03-06 04:22:26 +0100

    doc comments add

--- src/parser.rs
+++ src/parser.rs
@@ -47,7 +47,9 @@ pub fn parse<'source>(
 #[derive(Debug, Clone)]
 /// PklParser is the main parser struct, possessing the `parse` method to parse the tokens in the lexer.
 ///
-/// **IMPORTANT NOTE**: All parsing functions (in the entire library) are designed to operate on the next token from the lexer, except for the function that requires a token as a second argument.
+/// **IMPORTANT NOTE**: 
+/// All parsing functions (in the entire library) are designed to operate on the next token from the lexer, 
+/// except for the function that requires an optional token as a second argument, which will be consumed instead of consuming the next one.
 pub struct PklParser<'source> {
     pub statements: Vec<Statement<'source>>,
     lexer: PklLexer<'source>,
