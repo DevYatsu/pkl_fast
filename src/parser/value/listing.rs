10bc7748410acc2de55ab0317bab79eeb2a782ff
Yatsu <yatsu.dev@gmail.com>
2024-03-06 21:28:31 +0100

    listings parsing working

---
+++ src/parser/value/listing.rs
@@ -0,0 +1,130 @@
+use std::fmt::Display;
+
+use crate::{
+    parser::{
+        expression::{
+            basic::parse_basic_expr, complex::parse_complex_expr, parse_expr, Expression,
+        },
+        types::{parse_type, PklType},
+        utils::{assert_token_eq, expect_token, parse_identifier, retrieve_next_token},
+    },
+    prelude::{ParsingError, ParsingResult, PklLexer, PklToken},
+};
+
+use super::{parse_object, PklValue};
+#[derive(Debug, PartialEq, Clone)]
+pub enum ListingField<'a> {
+    Expression(Expression<'a>),
+    LocalVariable {
+        name: &'a str,
+        _type: Option<PklType<'a>>,
+        value: Expression<'a>,
+    },
+    DefaultObject(Expression<'a>),
+    AmendingElement {
+        index: Expression<'a>,
+        value: PklValue<'a>,
+    },
+}
+
+pub fn parse_listing_field<'source>(
+    lexer: &mut PklLexer<'source>,
+    next_token: PklToken<'source>,
+) -> ParsingResult<(ListingField<'source>, Option<PklToken<'source>>)> {
+    match next_token {
+        PklToken::Local => {
+            let name = parse_identifier(lexer)?;
+
+            match retrieve_next_token(lexer)? {
+                PklToken::EqualSign => {
+                    let (value, next) = parse_expr(lexer, Some(next_token))?;
+                    Ok((
+                        ListingField::LocalVariable {
+                            name,
+                            _type: None,
+                            value,
+                        },
+                        next,
+                    ))
+                }
+                PklToken::Colon => {
+                    let (_type, opt_token) = parse_type(lexer, None)?;
+                    assert_token_eq(lexer, opt_token, PklToken::EqualSign)?;
+                    let (value, next) = parse_expr(lexer, Some(next_token))?;
+                    Ok((
+                        ListingField::LocalVariable {
+                            name,
+                            _type: Some(_type),
+                            value,
+                        },
+                        next,
+                    ))
+                }
+                _ => Err(ParsingError::unexpected(lexer, "'=' or ':'".to_owned())),
+            }
+        }
+        PklToken::Default => {
+            expect_token(lexer, PklToken::OpenBracket)?;
+            let value = parse_object(lexer, None)?;
+
+            Ok((ListingField::DefaultObject(Expression::Value(value)), None))
+        }
+        PklToken::OpenParenthesis => {
+            let (expr, opt_token) = parse_basic_expr(lexer, None)?;
+            assert_token_eq(lexer, opt_token, PklToken::CloseParenthesis)?;
+
+            match expr {
+                Expression::ListIndexing { indexed, indexer } => {
+                    if indexed == "this" {
+                        expect_token(lexer, PklToken::OpenBracket)?;
+                        let value = parse_object(lexer, None)?;
+
+                        Ok((
+                            ListingField::AmendingElement {
+                                index: *indexer,
+                                value,
+                            },
+                            None,
+                        ))
+                    } else {
+                        let (expr, next) = parse_complex_expr(
+                            lexer,
+                            Expression::Parenthesised(Box::new(Expression::ListIndexing {
+                                indexed,
+                                indexer,
+                            })),
+                            None,
+                        )?;
+                        Ok((ListingField::Expression(expr), next))
+                    }
+                }
+                _ => {
+                    let (expr, next) =
+                        parse_complex_expr(lexer, Expression::Parenthesised(Box::new(expr)), None)?;
+                    Ok((ListingField::Expression(expr), next))
+                }
+            }
+        }
+        _ => {
+            let (expr, next) = parse_expr(lexer, Some(next_token))?;
+            Ok((ListingField::Expression(expr), next))
+        }
+    }
+}
+
+impl<'a> Display for ListingField<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ListingField::Expression(expr) => write!(f, "{expr}"),
+            ListingField::LocalVariable { name, value, _type } => {
+                if _type.is_some() {
+                    write!(f, "local {name}: {} = {value}", _type.clone().unwrap())
+                } else {
+                    write!(f, "local {name} = {value}")
+                }
+            }
+            ListingField::DefaultObject(x) => write!(f, "default {x}"),
+            ListingField::AmendingElement { index, value } => write!(f, "(this[{index}]) {value}"),
+        }
+    }
+}
