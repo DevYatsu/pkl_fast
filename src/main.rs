faab06964b3bac71f6a8ee6ed61896f6f952888c
Yatsu <yatsu.dev@gmail.com>
2024-08-16 17:05:59 +0200

    parsing 1300 lines of base.pkl code

--- src/main.rs
+++ src/main.rs
@@ -52,31 +52,54 @@ fn main() -> Result<(), (String, String, Option<String>)> {
             let mut result = parse(&src).unwrap();
 
             let file = result.next().unwrap().into_inner();
+
+            let mut doc_comment: Option<String> = None;
             for record in file {
                 match record.as_rule() {
                     Rule::stmt => {
                         for stmt in record.into_inner() {
                             match stmt.as_rule() {
-                                Rule::amends => {}
-                                Rule::import => {}
-                                Rule::extends => {}
-                                Rule::module => {}
-                                Rule::typealias => {}
-                                Rule::with_annotation => {
-                                    println!("{:?}", stmt.as_str())
+                                _ => {
+                                    // when managing the statement
+                                    // take the value inside the doc
+                                    // comment to attach it to attach
+                                    // it to the stmt if necessary
+                                    for stmt in stmt.into_inner() {
+                                        match stmt.as_rule() {
+                                            Rule::COMMENT => {
+                                                // println!("comment: {:?}", stmt.as_rule())
+                                            }
+                                            _ => {
+                                                // println!("any stmt: {:?}", stmt.as_rule());
+                                            }
+                                        }
+                                    }
                                 }
-                                Rule::with_doc_comment => {
-                                    println!("{:?}", stmt.as_str())
-                                }
-                                Rule::function => {}
-                                Rule::class => {}
-                                Rule::property => {}
-                                Rule::expr => {}
-                                _ => unreachable!(),
                             }
                         }
                     }
                     Rule::EOI => (),
+                    Rule::COMMENT => {
+                        for com in record.into_inner() {
+                            match com.as_rule() {
+                                Rule::doc_comment => {
+                                    doc_comment = Some(
+                                        doc_comment
+                                            .map(|mut x| {
+                                                x.push_str(com.as_str());
+                                                x
+                                            })
+                                            .unwrap_or(String::from(com.as_str())),
+                                    );
+                                }
+                                Rule::annotation => {
+                                    // take care of them here
+                                }
+                                Rule::line_comment | Rule::multiline_comment => (),
+                                _ => unreachable!(),
+                            }
+                        }
+                    }
                     _ => unreachable!(),
                 }
             }
